9ddbf94d79d1ae7c31337319cd1cb56257fefdde
Michael Iaria <8315738+mpiaria@users.noreply.github.com>
2020-12-06 21:19:27 -0600

    cdk synth works

--- infrastructure/src/main/java/dev/mostlycloudy/repobuilder/infrastructure/RepoBuilderWebsiteStack.java
+++ infrastructure/src/main/java/dev/mostlycloudy/repobuilder/infrastructure/RepoBuilderWebsiteStack.java
@@ -1,8 +1,10 @@
 package dev.mostlycloudy.repobuilder.infrastructure;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ecr.Repository;
 
 public class RepoBuilderWebsiteStack extends Stack {
     public RepoBuilderWebsiteStack(final Construct scope, final String id) {
@@ -13,5 +15,9 @@ public class RepoBuilderWebsiteStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
+        Repository.Builder.create(this, id + "container-repository")
+                .repositoryName("repo-builder-website")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
     }
 }
